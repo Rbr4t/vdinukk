c59e34b5f545064141ac7fe0586c18300fd37ea3
Rbr4t <95379567+Rbr4t@users.noreply.github.com>
2021-12-07 18:34:21 +0200

    Update vöödinukk_main_code.py

--- vöödinukk_main_code.py
+++ vöödinukk_main_code.py
@@ -4,6 +4,7 @@ pygame.init()
 screen = pygame.display.set_mode([800, 600])
 RUN = True
 
+#siia tuleb nüüd teha karakter ja teised defineeritavad muutujad, sest see siin on ainult algus
 
 while RUN:
     for e in pygame.event.get():
