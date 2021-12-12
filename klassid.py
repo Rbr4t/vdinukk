06d3d4723db69e823e5ffe402325f57d2c1ed12d
Rbr4t <robertkoor0@gmail.com>
2021-12-12 11:46:12 +0200

    Muudatused
    ehitasin ühe klassi, tegin natuke liikumist, ei tööta veel ja pole kindel miks. hei.txt saan lahti sest see oli ainult selleks et näha kas te ikka saate muuta asju jne.

--- klassid.py
+++ klassid.py
@@ -1,6 +1,13 @@
-#siia tulevad meie mängu klassid, pärast impordime need main faili
-class Tegelane:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    
+#siia tulevad meie mängu klassid, pärast impordime need main faili VÕI TEGELIKULT MITTE
+class Player:
+    def __init__(self):
+        self.x = 320
+        self.y = 240
+        self.vx = 0
+        self.vy = 0
+        self.img = pygame.image.load(player_image)
+    def update(self, dt):
+        self.x += self.vx * dt
+        self.y += self.vy * dt
+    def draw(self, s):
+        s.blit(self.img, [self.x - self.img.get_width() / 2, self.y - self.img.get_height() / 2])
--- vöödinukk_main_code.py
+++ vöödinukk_main_code.py
@@ -1,15 +1,56 @@
 import pygame, sys
+#from klassid.py import *
+
+playerpilt = "t_front1"
 
 pygame.init()
 screen = pygame.display.set_mode([800, 600])
 RUN = True
 
-#siia tuleb nüüd teha karakter ja teised defineeritavad muutujad, sest see siin on ainult algus
+class Player:
+    def __init__(self):
+        self.x = 320
+        self.y = 240
+        self.vx = 0
+        self.vy = 0
+        self.img = pygame.image.load(playerpilt)  #<----- see siin veel ei tööta
+    def update(self, dt):
+        self.x += self.vx * dt
+        self.y += self.vy * dt
+    def draw(self, s):
+        s.blit(self.img, [self.x - self.img.get_width() / 2, self.y - self.img.get_height() / 2])
+        
+player = Player()
+
+speed = 10
+kell = pygame.time.Clock()
 
 while RUN:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             RUN = False
-        if e.type == pygame.
+        if e.type == pygame.KEYDOWN: #Kui vajutatakse alla nuppu
+            if e.key == K_UP:
+                player.vy += speed
+            if e.key == K_DOWN:
+                player.vy -= speed
+            if e.key == K_LEFT:
+                player.vx -= speed
+            if e.key == K_RIGHT:
+                player.vx += speed
+        if e.type == pygame.UP: #kui enam ei vajutata seda
+            if e.key == K_UP:
+                player.vy -= speed
+            if e.key == K_DOWN:
+                player.vy += speed
+            if e.key == K_LEFT:
+                player.vx += speed
+            if e.key == K_RIGHT:
+                player.vx -= speed
+    
+    screen.fill([255, 255, 255])
+    player.update(dt) #uuendame asukohta
+    player.draw(screen) #joonistame ekraanile
+    pygame.display.flip()
 
 pygame.quit()
