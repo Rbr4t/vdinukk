dfe8abaccc2e8391dd4a6bcc66010d1a9af2f9dd
Rbr4t <robertkoor0@gmail.com>
2021-12-14 23:08:30 +0200

    Animeerimine 2
    Tegelane liigub animeeritult, aga see on
    1) liiga kiire - vajaks parandamist
    2) ta on "chunky" - vajaks parandamist
    3) ebaloomulik - kannatab oodata

--- vöödinukk_main_code.py
+++ vöödinukk_main_code.py
@@ -1,24 +1,6 @@
-import pygame, sys
+import pygame
 
 #FUNKTSIOONID
-
-def X():
-    left = False
-    right = False
-    up = False
-    down = False
-#tuleb piltide animeerimine:
-
-walkLeft = [pygame.image.load("characters/tüdruk_left/t_left1.png"),
-            pygame.image.load("characters/tüdruk_left/t_left2.png"), pygame.image.load("characters/tüdruk_left/t_left3.png"),
-            pygame.image.load("characters/tüdruk_left/t_left4.png")]
-walkRight = [pygame.image.load("characters/tüdruk_right/t_right1.png"), pygame.image.load("characters/tüdruk_right/t_right2.png"),
-             pygame.image.load("characters/tüdruk_right/t_right3.png"), pygame.image.load("characters/tüdruk_right/t_right4.png")]
-walkFront = [pygame.image.load("characters/tüdruk_front/t_front1.png"), pygame.image.load("characters/tüdruk_front/t_front2.png"),
-             pygame.image.load("characters/tüdruk_front/t_front3.png"), pygame.image.load("characters/tüdruk_front/t_front4.png")]
-walkBack = [pygame.image.load("characters/tüdruk_back/t_back1.png"), pygame.image.load("characters/tüdruk_back/t_back2.png"),
-            pygame.image.load("characters/tüdruk_back/t_back3.png"), pygame.image.load("characters/tüdruk_back/t_back4.png")]
-playerpilt = "characters/tüdruk_front/t_front1.png"
 
 def redrawWindow():
     global walkCount
@@ -38,19 +20,29 @@ def redrawWindow():
         walkCount += 1
     elif down:
         screen.blit(walkFront[walkCount//8], (player.x, player.y))
+        walkCount += 1
     else:
-        screen.blit(screen, (player.x, player.y))
+        player.draw(screen)
         
     pygame.display.flip()
+    
+#ANIMEERIMINE
 
-pygame.init()
+#vajalikud pildid võetud kaustast
+walkLeft = [pygame.image.load("characters/tüdruk_left/t_left1.png"),
+            pygame.image.load("characters/tüdruk_left/t_left2.png"), pygame.image.load("characters/tüdruk_left/t_left3.png"),
+            pygame.image.load("characters/tüdruk_left/t_left4.png")]
+walkRight = [pygame.image.load("characters/tüdruk_right/t_right1.png"), pygame.image.load("characters/tüdruk_right/t_right2.png"),
+             pygame.image.load("characters/tüdruk_right/t_right3.png"), pygame.image.load("characters/tüdruk_right/t_right4.png")]
+walkFront = [pygame.image.load("characters/tüdruk_front/t_front1.png"), pygame.image.load("characters/tüdruk_front/t_front2.png"),
+             pygame.image.load("characters/tüdruk_front/t_front3.png"), pygame.image.load("characters/tüdruk_front/t_front4.png")]
+walkBack = [pygame.image.load("characters/tüdruk_back/t_back1.png"), pygame.image.load("characters/tüdruk_back/t_back2.png"),
+            pygame.image.load("characters/tüdruk_back/t_back3.png"), pygame.image.load("characters/tüdruk_back/t_back4.png")]
+playerpilt = "characters/tüdruk_front/t_front1.png"
+char = pygame.image.load(playerpilt)
+#KLASSID
 
-bg = pygame.Surface((640, 480)) #vajalik hilisemaks animeerimiseks
-screen = pygame.display.set_mode([800, 600])
-
-RUN = True
-
-#tegin praegu selle klassi, sest ilma ma ei saa alustada mängu liikumise tegemisega
+#playeri klass, mainklass
 class Player:
     def __init__(self):
         self.x = 320
@@ -63,6 +55,15 @@ class Player:
         self.y += self.vy * dt
     def draw(self, s):
         s.blit(self.img, [self.x - self.img.get_width() / 2, self.y - self.img.get_height() / 2])
+        
+#Mäng hakkab siit
+pygame.init()
+
+bg = pygame.Surface((0, 0)) #vajalik hilisemaks animeerimiseks
+
+screen = pygame.display.set_mode([800, 600])
+
+RUN = True
         
 player = Player()
 
@@ -77,6 +78,7 @@ walkCount= 0 #see muutuja on siin selle jaoks et hiljem saaks
 kell = pygame.time.Clock()
 
 while RUN:
+    
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             RUN = False
@@ -110,22 +112,34 @@ while RUN:
         if e.type == pygame.KEYUP: #kui enam ei vajutata seda
             if e.key == pygame.K_UP:
                 player.vy += speed
-                X()
+                left = False
+                right = False
+                up = False
+                down = False
             if e.key == pygame.K_DOWN:
                 player.vy -= speed
-                X()
+                left = False
+                right = False
+                up = False
+                down = False
             if e.key == pygame.K_LEFT:
                 player.vx += speed
-                X()
+                left = False
+                right = False
+                up = False
+                down = False
             if e.key == pygame.K_RIGHT:
                 player.vx -= speed
-                X()
+                left = False
+                right = False
+                up = False
+                down = False
     
-    dt = kell.tick()/1000
-    screen.fill([255, 255, 255])
+    dt = kell.tick()/500
     
     player.update(dt) #uuendame asukohta
     
     redrawWindow()
-
+    screen.fill([255, 255, 255])
+    
 pygame.quit()
